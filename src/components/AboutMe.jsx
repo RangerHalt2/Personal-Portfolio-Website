a98b80af167f9afdfa62c79d0b8fc8c23165dfba
RangerHalt2 <141537190+RangerHalt2@users.noreply.github.com>
2025-08-31 18:13:09 -0400

    updating resume with class resume

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -37,7 +37,7 @@ export const AboutSection = () => {
               </a>
               <a
                 href="../public/files/Baysinger_CV.pdf"
-                download={"Logan_Baysinger_Resume.pdf"}
+                download={"Resume_Baysinger_Logan.docx"}
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
