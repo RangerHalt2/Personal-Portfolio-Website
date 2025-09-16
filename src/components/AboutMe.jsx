e51cbed7edc43cb6c7acd3705c9080cd01dec15a
RangerHalt2 <141537190+RangerHalt2@users.noreply.github.com>
2025-09-16 12:42:01 -0400

    fixing file path for real this time

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -36,7 +36,7 @@ export const AboutSection = () => {
                 Get In Touch With Me
               </a>
               <a
-                href="../../public/files/Baysinger_Logan_Resume.pdf"
+                href="/files/Baysinger_Logan_Resume.pdf"
                 download={"Baysinger_Logan_Resume.pdf"}
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
